6a6b77e4df75e3b8fd78f3b0acad240d6082641f
Piarre <Pierre@ideestore.fr>
2022-12-15 13:11:30 +0100

    full JS -> TS

---
+++ src/constants/yArgs.ts
@@ -0,0 +1,27 @@
+export const yargsOptions: any = {
+  mcversion: {
+    type: "string",
+    demandOption: false,
+    alias: "v",
+    description: "Minecraft server version's.",
+  },
+  spigot: {
+    type: "boolean",
+    demandOption: false,
+    alias: "s",
+    description: "If you want to use spigot server.",
+  },
+  sdelete: {
+    type: "con",
+    demandOption: false,
+    alias: "d",
+    description: "Delete the server.",
+  },
+  folderName: {
+    type: "string",
+    demandOption: false,
+    alias: "f",
+    description: "The name of the server folder.",
+    default: "server",
+  },
+};
--- src/utils/returnMessageHandler.ts
+++ src/utils/returnMessageHandler.ts
@@ -8,20 +8,20 @@ export enum MessageType {
 export default function Message(
   message: string,
   type: MessageType
-): TypeError | string | undefined {
+): TypeError | void | undefined {
   if (!(type in MessageType))
     return new TypeError(
       "Only ERROR, SUCCES, WARNING and PROCESSING types are allowed."
     );
   switch (type) {
     case MessageType.ERROR:
-      return `\x1b[41mERROR\x1b[0m ${message}`;
+      return console.log(`\x1b[41mERROR\x1b[0m ${message}`);
     case MessageType.SUCCESS:
-      return `\x1b[42mSUCCESS\x1b[0m ${message}`;
+      return console.log(`\x1b[42mSUCCESS\x1b[0m ${message}`);
     case MessageType.WARNING:
-      return `\x1b[43mWARNING\x1b[0m ${message}`;
+      return console.log(`\x1b[43mWARNING\x1b[0m ${message}`);
     case MessageType.PROCESSING:
-      return `\x1b[44mPROCESSING\x1b[0m ${message}`;
+      return console.log(`\x1b[44mPROCESSING\x1b[0m ${message}`);
     default:
       break;
   }
